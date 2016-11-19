47fd835cb56ca1fb04608a0053e1645e97c004a0
yuhuimzz <395544527@qq.com>
2016-11-19 21:41:04 +0800

    question更新2（sql文件忘记w上传啦）

--- smx.sql
+++ smx.sql
@@ -1,27 +1,24 @@
--- MySQL dump 10.13  Distrib 5.5.28, for Win64 (x86)
---
--- Host: localhost    Database: smx
--- ------------------------------------------------------
--- Server version	5.5.28
+/*
+Navicat MySQL Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+Source Server         : smx
+Source Server Version : 50528
+Source Host           : localhost:3306
+Source Database       : smx
 
---
--- Table structure for table `account`
---
+Target Server Type    : MYSQL
+Target Server Version : 50528
+File Encoding         : 65001
 
+Date: 2016-11-19 21:40:14
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for account
+-- ----------------------------
 DROP TABLE IF EXISTS `account`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account` (
   `userId` int(32) NOT NULL AUTO_INCREMENT,
   `phoneNumber` varchar(11) DEFAULT NULL,
@@ -36,25 +33,19 @@ CREATE TABLE `account` (
   PRIMARY KEY (`userId`),
   KEY `name` (`userName`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `account`
---
+-- ----------------------------
+-- Records of account
+-- ----------------------------
+INSERT INTO `account` VALUES ('1', '18840824301', '123456', '2', '姚晗', '大黑山双语职业学校', '大一', '23', '大黑山村1幢101', 'http://smxbucket-10068625.cos.myqcloud.com/%E7%83%AD%E9%97%A8%E8%AF%BE%E7%A8%8B%E5%A4%B4%E5%83%8F.png');
+INSERT INTO `account` VALUES ('7', '1', '1', '1', '姚同学', '大连第二高中', '高二', '23', '大黑山村2幢202', 'http://smxbucket-10068625.cos.myqcloud.com/%E7%83%AD%E9%97%A8%E8%AF%BE%E7%A8%8B%E5%A4%B4%E5%83%8F.png');
+INSERT INTO `account` VALUES ('8', '2', '2', '0', '姓名2', '大连第三高中', '高三', '12', '大黑山村3幢303', 'http://smxbucket-10068625.cos.myqcloud.com/%E7%83%AD%E9%97%A8%E8%AF%BE%E7%A8%8B%E5%A4%B4%E5%83%8F.png');
+INSERT INTO `account` VALUES ('9', '18697023568', '201492170', '2', '於晖', '大黑山职业中专', '大二', '18', '大黑山村4幢404', 'http://smxbucket-10068625.cos.myqcloud.com/%E7%83%AD%E9%97%A8%E8%AF%BE%E7%A8%8B%E5%A4%B4%E5%83%8F.png');
 
-LOCK TABLES `account` WRITE;
-/*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES (1,'18840824301','123456',2,'姚晗','大黑山双语职业学校','大一','23','大黑山村1幢101','http://smxbucket-10068625.cos.myqcloud.com/%E7%83%AD%E9%97%A8%E8%AF%BE%E7%A8%8B%E5%A4%B4%E5%83%8F.png'),(7,'1','1',1,'姚同学','大连第二高中','高二','23','大黑山村2幢202','http://smxbucket-10068625.cos.myqcloud.com/%E7%83%AD%E9%97%A8%E8%AF%BE%E7%A8%8B%E5%A4%B4%E5%83%8F.png'),(8,'2','2',0,'姓名2','大连第三高中','高三','12','大黑山村3幢303','http://smxbucket-10068625.cos.myqcloud.com/%E7%83%AD%E9%97%A8%E8%AF%BE%E7%A8%8B%E5%A4%B4%E5%83%8F.png'),(9,'18697023568','201492170',2,'於晖','大黑山职业中专','大二','18','大黑山村4幢404','http://smxbucket-10068625.cos.myqcloud.com/%E7%83%AD%E9%97%A8%E8%AF%BE%E7%A8%8B%E5%A4%B4%E5%83%8F.png');
-/*!40000 ALTER TABLE `account` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `answer`
---
-
+-- ----------------------------
+-- Table structure for answer
+-- ----------------------------
 DROP TABLE IF EXISTS `answer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `answer` (
   `questionId` int(32) unsigned NOT NULL AUTO_INCREMENT,
   `answerId` int(32) NOT NULL,
@@ -63,25 +54,16 @@ CREATE TABLE `answer` (
   `answerTime` datetime DEFAULT NULL,
   PRIMARY KEY (`questionId`,`answerId`,`userId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `answer`
---
+-- ----------------------------
+-- Records of answer
+-- ----------------------------
+INSERT INTO `answer` VALUES ('1', '1', '1', 'qwdqw', '2016-10-28 17:23:00');
 
-LOCK TABLES `answer` WRITE;
-/*!40000 ALTER TABLE `answer` DISABLE KEYS */;
-INSERT INTO `answer` VALUES (1,1,1,'qwdqw','2016-10-28 17:23:00');
-/*!40000 ALTER TABLE `answer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `course`
---
-
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
 DROP TABLE IF EXISTS `course`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `course` (
   `courseId` int(32) NOT NULL AUTO_INCREMENT,
   `userId` int(32) DEFAULT NULL,
@@ -91,122 +73,83 @@ CREATE TABLE `course` (
   `courseContent` text,
   PRIMARY KEY (`courseId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `course`
---
+-- ----------------------------
+-- Records of course
+-- ----------------------------
+INSERT INTO `course` VALUES ('1', '1', '高三数学', '周六早上|9:00-12:00', '高三', '三角函数');
+INSERT INTO `course` VALUES ('3', '1', '高三语文', '周六早上|9:00-12:00', '高三', '阅读理解');
+INSERT INTO `course` VALUES ('6', '9', '高二数学', '周六早上|9:00-12:00', '高二', '圆锥曲线');
 
-LOCK TABLES `course` WRITE;
-/*!40000 ALTER TABLE `course` DISABLE KEYS */;
-INSERT INTO `course` VALUES (1,1,'高三数学','周六早上|9:00-12:00','高三','三角函数'),(3,1,'高三语文','周六早上|9:00-12:00','高三','阅读理解'),(6,9,'高二数学','周六早上|9:00-12:00','高二','圆锥曲线');
-/*!40000 ALTER TABLE `course` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `joincourse`
---
-
+-- ----------------------------
+-- Table structure for joincourse
+-- ----------------------------
 DROP TABLE IF EXISTS `joincourse`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `joincourse` (
   `userId` int(32) NOT NULL,
   `courseId` int(32) DEFAULT NULL,
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `joincourse`
---
+-- ----------------------------
+-- Records of joincourse
+-- ----------------------------
+INSERT INTO `joincourse` VALUES ('7', '3');
+INSERT INTO `joincourse` VALUES ('8', '1');
 
-LOCK TABLES `joincourse` WRITE;
-/*!40000 ALTER TABLE `joincourse` DISABLE KEYS */;
-INSERT INTO `joincourse` VALUES (7,3),(8,1);
-/*!40000 ALTER TABLE `joincourse` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `onlineteacher`
---
-
+-- ----------------------------
+-- Table structure for onlineteacher
+-- ----------------------------
 DROP TABLE IF EXISTS `onlineteacher`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `onlineteacher` (
   `userId` int(32) NOT NULL,
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `onlineteacher`
---
+-- ----------------------------
+-- Records of onlineteacher
+-- ----------------------------
 
-LOCK TABLES `onlineteacher` WRITE;
-/*!40000 ALTER TABLE `onlineteacher` DISABLE KEYS */;
-/*!40000 ALTER TABLE `onlineteacher` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `question`
---
-
+-- ----------------------------
+-- Table structure for question
+-- ----------------------------
 DROP TABLE IF EXISTS `question`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `question` (
-  `questionId` int(32) NOT NULL,
+  `questionId` int(32) NOT NULL AUTO_INCREMENT,
   `userId` int(32) NOT NULL,
   `questionTitle` varchar(255) DEFAULT NULL,
   `questionContent` text,
   `questionTime` datetime DEFAULT NULL,
   PRIMARY KEY (`questionId`,`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `question`
---
+-- ----------------------------
+-- Records of question
+-- ----------------------------
+INSERT INTO `question` VALUES ('1', '1', '等边三角怎么证', '等边三角形在什么条件下可以证明成立', '2016-11-19 16:44:37');
+INSERT INTO `question` VALUES ('2', '1', '等腰三角形怎么证', '等腰三角形在什么条件下成立', '2016-11-16 16:45:35');
 
-LOCK TABLES `question` WRITE;
-/*!40000 ALTER TABLE `question` DISABLE KEYS */;
-/*!40000 ALTER TABLE `question` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `teacher`
---
-
+-- ----------------------------
+-- Table structure for teacher
+-- ----------------------------
 DROP TABLE IF EXISTS `teacher`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `teacher` (
   `teacherId` int(10) NOT NULL,
   `goodCourse` varchar(255) DEFAULT NULL,
   `selfIntroduction` text,
   PRIMARY KEY (`teacherId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `teacher`
---
+-- ----------------------------
+-- Records of teacher
+-- ----------------------------
+INSERT INTO `teacher` VALUES ('1', '数学，英语', '个人简介个人简介个人简介个人简介个人简介个人简介个人简介个人简介');
+INSERT INTO `teacher` VALUES ('9', '物理，化学', '个人简介个人简介');
 
-LOCK TABLES `teacher` WRITE;
-/*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
-INSERT INTO `teacher` VALUES (1,'数学，英语','个人简介个人简介个人简介个人简介个人简介个人简介个人简介个人简介'),(9,'物理，化学','个人简介个人简介');
-/*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `video`
---
-
+-- ----------------------------
+-- Table structure for video
+-- ----------------------------
 DROP TABLE IF EXISTS `video`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `video` (
   `videoId` int(30) unsigned NOT NULL AUTO_INCREMENT,
   `authorId` int(11) DEFAULT NULL,
@@ -219,48 +162,22 @@ CREATE TABLE `video` (
   `videoContent` text CHARACTER SET utf8,
   PRIMARY KEY (`videoId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `video`
---
+-- ----------------------------
+-- Records of video
+-- ----------------------------
+INSERT INTO `video` VALUES ('1', '1', 'http://smxbucket-10068625.cos.myqcloud.com/%E8%A7%86%E9%A2%91%E5%9B%BE%E7%89%87.png', 'http://smxbucket-10068625.cos.myqcloud.com/%E5%B0%8FCC%EF%BC%88%E3%80%8A%E5%B0%8F%E8%8B%B9%E6%9E%9C%E3%80%8BC%E8%AF%AD%E8%A8%80%E7%89%88-for%E5%A4%A7%E8%BF%9E%E7%90%86%E5%B7%A5%E5%A4%A7%E5%AD%A6%E8%BD%AF%E4%BB%B6%E5%AD%A6%E9%99%A2%EF%BC%89_%E9%AB%98%E6%B8%85.mp4', '开箱实拍', '00:15:15', '13', '高中数学', '三角函数和数列，统计概率，立体几何，圆锥曲线');
 
-LOCK TABLES `video` WRITE;
-/*!40000 ALTER TABLE `video` DISABLE KEYS */;
-INSERT INTO `video` VALUES (1,1,'http://smxbucket-10068625.cos.myqcloud.com/%E8%A7%86%E9%A2%91%E5%9B%BE%E7%89%87.png','http://smxbucket-10068625.cos.myqcloud.com/%E5%B0%8FCC%EF%BC%88%E3%80%8A%E5%B0%8F%E8%8B%B9%E6%9E%9C%E3%80%8BC%E8%AF%AD%E8%A8%80%E7%89%88-for%E5%A4%A7%E8%BF%9E%E7%90%86%E5%B7%A5%E5%A4%A7%E5%AD%A6%E8%BD%AF%E4%BB%B6%E5%AD%A6%E9%99%A2%EF%BC%89_%E9%AB%98%E6%B8%85.mp4','开箱实拍','00:15:15',13,'高中数学','三角函数和数列，统计概率，立体几何，圆锥曲线');
-/*!40000 ALTER TABLE `video` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `videotag`
---
-
+-- ----------------------------
+-- Table structure for videotag
+-- ----------------------------
 DROP TABLE IF EXISTS `videotag`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `videotag` (
   `videoId` int(32) NOT NULL,
   `tagName` varchar(255) NOT NULL,
   PRIMARY KEY (`videoId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `videotag`
---
-
-LOCK TABLES `videotag` WRITE;
-/*!40000 ALTER TABLE `videotag` DISABLE KEYS */;
-/*!40000 ALTER TABLE `videotag` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-11-19 14:14:24
+-- ----------------------------
+-- Records of videotag
+-- ----------------------------
