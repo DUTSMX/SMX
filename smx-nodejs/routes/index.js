e43e883a6a56c0702a93e9a86022dd9a94c2da47
姚晗 <937860151@qq.com>
2016-12-01 20:50:55 +0800

    首页iframe 顶部菜单

--- smx-nodejs/routes/index.js
+++ smx-nodejs/routes/index.js
@@ -5,6 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/main',function (req,res) {
+  res.render('main',{});
+})
 
 /*
 * unuse
