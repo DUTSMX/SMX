c2c86c4c9867c30854dfee4ae02dff0669a8f501
姚晗 <937860151@qq.com>
2016-12-02 12:18:38 +0800

    顶部标题栏

---
+++ smx-nodejs/public/javascripts/smx.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".ad_Images").slick({
+        autoplay: "true",
+        autoplaySpeed: "4000"
+    });
+});
+function goBack(){
+    window.history.back()
+}
