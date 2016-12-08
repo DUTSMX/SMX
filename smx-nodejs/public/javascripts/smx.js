64fc85efd3af73151c847e043d40891b9c81c65d
姚晗 <937860151@qq.com>
2016-12-08 12:12:06 +0800

    js位置

--- smx-nodejs/public/javascripts/smx.js
+++ smx-nodejs/public/javascripts/smx.js
@@ -1,3 +1,10 @@
+var xmlhttp;
+if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+}else{// code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+/*轮播图*/
 $(document).ready(function () {
     $(".ad_Images").slick({
         autoplay: "true",
@@ -6,4 +13,180 @@ $(document).ready(function () {
 });
 function goBack(){
     window.history.back()
+}
+/*修改密码*/
+function changePassword() {
+    var oldPassword = document.getElementById("oldPassword").value;
+    var password = document.getElementById("password").value;
+    var passwordAgain = document.getElementById("passwordAgain").value;
+    if(oldPassword.length == 0){
+        document.getElementById("hint").innerHTML = "旧密码不能为空";
+    }if(password.length == 0){
+        document.getElementById("hint").innerHTML = "新密码不能为空";
+    }else if(password !=passwordAgain){//发送登录请求;
+        document.getElementById("hint").innerHTML = "新密码不一致";
+    }else{
+        xmlhttp.open("GET", "/users/changePassword?oldPassword=" + oldPassword + "&newPassword=" + password, true);
+        xmlhttp.send();
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            var str = xmlhttp.responseText;//得到服务器响应
+            document.getElementById("hint").innerHTML=str;
+            if(str == "修改成功"){
+                window.location.href="/users/loginPage";
+            }
+        }
+    }
+}
+/* 加入课程 */
+function joinCourse() {
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            $("#join_course").text(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET","/course/joinCourse?courseId=<%=courseId%>",true);
+    xmlhttp.send();
+}
+function createCourse () {
+    console.log("请求发送成功");
+    var courseName = document.getElementById("courseName").value;
+    var courseDate = document.getElementById("courseDate").value;
+    var beginTime = document.getElementById("beginTime").value;
+    var finshTime = document.getElementById("finshTime").value;
+    var courseTime = document.getElementById("courseTime").value;
+    var objectOriented = document.getElementById("objectOriented").value;
+    var courseContent = document.getElementById("courseContent").value;
+    var url = "/course/createCourse?courseName=" + courseName + "&courseDate=" + courseDate + "&beginTime=" + beginTime + "" +
+        "&finshTime=" + finshTime + "&courseTime=" + courseTime + "&objectOriented=" + objectOriented + "&courseContent=" + courseContent;
+    xmlhttp.open("GET",url,true)
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            str = xmlhttp.responseText;//得到服务器响应
+            document.getElementById("hint").innerHTML = str;//显示提示信息
+            if (str == "创建成功") {
+                window.location.href = "/course/course";//跳转到主页
+            }
+        }
+    }
+}
+function forgetPassword() {
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var checkCode = document.getElementById("checkCode").value;
+    var password = document.getElementById("password").value;
+    var passwordAgain = document.getElementById("passwordAgain").value;
+    if(phoneNumber.length == 0){
+        document.getElementById("hint").innerHTML = "手机号不能为空";
+    }else if(!(/^1[34578]\d{9}$/.test(phoneNumber))){
+        document.getElementById("hint").innerHTML = "手机号格式不正确";
+    }else if(password.length == 0){
+        document.getElementById("hint").innerHTML = "密码不能为空";
+    }else if(password !=passwordAgain){//发送登录请求;
+        document.getElementById("hint").innerHTML = "密码不一致";
+    }else{
+        xmlhttp.open("GET", "/users/forgetPassword?phoneNumber=" + phoneNumber + "&password=" + password, true);
+        xmlhttp.send();
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            var str = xmlhttp.responseText;//得到服务器响应
+            document.getElementById("hint").innerHTML=str;
+            if(str == "修改成功"){
+                window.location.href="/users/loginPage";
+            }
+        }
+    }
+}
+
+function login() {
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var password = document.getElementById("password").value;
+    if(phoneNumber.length == 0){
+        document.getElementById("hint").innerHTML = "手机号不能为空";
+    }else if(!(/^1[34578]\d{9}$/.test(phoneNumber))){
+        document.getElementById("hint").innerHTML = "手机号格式不正确";
+    }else if(password.length == 0){
+        document.getElementById("hint").innerHTML = "密码不能为空";
+    }else{//发送登录请求
+        xmlhttp.open("GET", "/users/login?phoneNumber=" + phoneNumber + "&password=" + password, true);
+        xmlhttp.send();
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            str = xmlhttp.responseText;//得到服务器响应
+            document.getElementById("hint").innerHTML = str;//显示提示信息
+            if(str == "登录成功"){
+                window.location.href = "/course/course";//跳转到主页
+            }
+        }
+    }
+}
+function statusSwitch() {
+    var status = '<%=status%>'
+    console.log("status:"+status);
+    if(status == 1){
+        status = 0;
+        document.getElementById("question_status").innerHTML = "状态：关";
+        xmlhttp.open('GET',"/users/questionStatus?userId=<%=userId%>&status=0");
+
+    }else{
+        status = 1;
+        document.getElementById("question_status").innerHTML = "状态：开";
+        xmlhttp.open('GET',"/users/questionStatus?userId=<%=userId%>&status=1");
+    }
+    xmlhttp.send();
+}
+
+function register() {
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var checkCode = document.getElementById("checkCode").value;
+    var password = document.getElementById("password").value;
+    var passwordAgain = document.getElementById("passwordAgain").value;
+    if(phoneNumber.length == 0){
+        document.getElementById("hint").innerHTML = "手机号不能为空";
+    }else if(!(/^1[34578]\d{9}$/.test(phoneNumber))){
+        document.getElementById("hint").innerHTML = "手机号格式不正确";
+    }else if(password.length == 0){
+        document.getElementById("hint").innerHTML = "密码不能为空";
+    }else if(password !=passwordAgain){//发送登录请求;
+        document.getElementById("hint").innerHTML = "密码不一致";
+    }else{
+        xmlhttp.open("GET", "/users/register?phoneNumber=" + phoneNumber + "&password=" + password, true);
+        xmlhttp.send();
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            var str = xmlhttp.responseText;//得到服务器响应
+            document.getElementById("hint").innerHTML=str;
+            if(str == "注册成功"){
+                window.location.href="/course/course";
+            }
+        }
+    }
+}
+
+function registerTeacher() {
+    console.log("请求发送成功");
+    var goodCourse = document.getElementById("goodCourse").value;
+    var selfIntro = document.getElementById("selfIntro").value;
+    var url = "registerTeacher?goodCourse=" +goodCourse+ "&selfIntro="+ selfIntro;
+    xmlhttp.open("GET",url,true)
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            str = xmlhttp.responseText;//得到服务器响应
+            document.getElementById("hint").innerHTML = str;//显示提示信息
+            if (str == "申请成功") {
+                window.location.href = "/course/course";//跳转到主页
+            }
+        }
+    }
 }
--- smx-nodejs/routes/users.js
+++ smx-nodejs/routes/users.js
@@ -17,14 +17,7 @@ router.get('/mine',function (req,res) {
         })
     }
 })
-<<<<<<< HEAD
-router.get('/personDetail',function (req,res) {
-    res.render('personDetail',{});
-})
 router.get('/registerTeacherPage',function (req,res) {
-=======
-router.get('/registerTeacher',function (req,res) {
->>>>>>> 3b4b0e4ca3cabd0c94862275d634bbf424e6e2e2
     res.render('registerTeacher',{});
 })
 router.get('/createCourse',function (req,res) {
@@ -150,10 +143,6 @@ router.get('/registerTeacher',function (req,res) {
         })
     }
 })
-<<<<<<< HEAD
-
-
-=======
 router.get("/personDetail",function (req,res) {
     var userId=req.session.userId;
     console.log("userId:"+userId)
@@ -166,7 +155,6 @@ router.get("/personDetail",function (req,res) {
         })
     }
 })
->>>>>>> 3b4b0e4ca3cabd0c94862275d634bbf424e6e2e2
 /*
 * unuse
 
