7fa9d12ad7a4ba8e245c6e922d7bf01cb901fefa
姚晗 <937860151@qq.com>
2016-12-24 13:28:04 +0800

    sql 错误、

--- smx-nodejs/db/courseDBHelper.js
+++ smx-nodejs/db/courseDBHelper.js
@@ -33,7 +33,7 @@ exports.getCourseById = function (userId, callback) {
         "a.userName as teacherName, " +
         "a.userSchool as teacherSchool, " +
         "a.userGrade as teacherGrade " +
-        "FROM ((joinCourse j INNER JOIN course c ON j.courseId = c.courseId) INNER JOIN account a ON c.userId = a.userId) on c.courseDate >= curdate()" ;
+        "FROM ((joinCourse j INNER JOIN course c ON j.courseId = c.courseId) INNER JOIN account a ON c.userId = a.userId) WHERE c.courseDate >= curdate()" ;
     var sql2 = "SELECT c.courseId, " +
         "c.courseName, " +
         "c.courseTime, " +
@@ -41,15 +41,15 @@ exports.getCourseById = function (userId, callback) {
         "a.userSchool as teacherSchool, " +
         "a.userGrade as teacherGrade, " +
         "a.userName as teacherName " +
-        "FROM course c JOIN account a ON c.userId = a.userId and c.courseDate>= curdate()" +
-        "WHERE c.userId = " + userId ;
+        "FROM course c JOIN account a ON c.userId = a.userId " +    ``
+        "WHERE  and c.courseDate >= curdate() and c.userId = " + userId ;
     if (users.judgeRole(userId, function (rows) {
         // console.log("length:" + rows.length);
         if (rows && rows.length > 0) {
             var role = rows[0].role;
             console.log("role:"+role)
             if (role == 0 || role == 1) {//学生
-                // console.log(sql1);
+                console.log("sql:"+sql1);
                 conn.query(sql1, function (err, rows, fields) {
                     if (err) {
                         console.log(err);
