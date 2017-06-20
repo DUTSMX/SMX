3051ab0ad917001c084222d4e53bb8565fafa002
姚晗 <937860151@qq.com>
2017-06-20 21:29:04 +0800

    教师 加盟店长

--- newAdmin/routes/index.js
+++ newAdmin/routes/index.js
@@ -5,11 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-router.get('/student',function (req,res,next) {
-  res.render('student');
-})
 router.get('/admin',function (req,res,next) {
   res.render('admin');
+})
+router.get('/joinManager',function (req,res,next) {
+  res.render('joinManager');
 })
 router.get('/joinList',function (req,res,next) {
   res.render('joinList');
@@ -17,11 +17,29 @@ router.get('/joinList',function (req,res,next) {
 router.get('/joinDetail',function (req,res,next) {
   res.render('joinDetail');
 })
+router.get('/joinReception',function (req,res,next) {
+  res.render('joinReception');
+})
+router.get('/joinReceptionStudentDetail',function (req,res,next) {
+  res.render('joinReceptionStudentDetail');
+})
+router.get('/studentCourse',function (req,res,next) {
+  res.render('studentCourse');
+})
 router.get('/studentList',function (req,res,next) {
   res.render('studentList');
 })
 router.get('/studentDetail',function (req,res,next) {
   res.render('studentDetail');
+})
+router.get('/studentCourseDetail',function (req,res,next) {
+  res.render('studentCourseDetail')
+})
+router.get('/teacherCourse',function (req,res,next) {
+  res.render('teacherCourse')
+})
+router.get('/teacherCourseDetail',function (req,res,next) {
+  res.render('teacherCourseDetail')
 })
 router.get('/teacherList',function (req,res,next) {
   res.render('teacherList');
