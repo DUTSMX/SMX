86100055e5518f826049e9139e85ee0bbd65a40b
姚晗 <937860151@qq.com>
2017-06-21 21:07:46 +0800

    前台

--- newAdmin/routes/index.js
+++ newAdmin/routes/index.js
@@ -5,29 +5,37 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-router.get('/admin',function (req,res,next) {
-  res.render('admin');
+router.get('/joinManagerReport',function (req,res,next) {
+  res.render('joinManagerReport');
 })
-router.get('/joinManager',function (req,res,next) {
-  res.render('joinManager');
+router.get('/joinManagerDetail',function (req,res,next) {
+  res.render('joinManagerDetail');
 })
-router.get('/joinList',function (req,res,next) {
-  res.render('joinList');
+
+router.get('/joinReceptionDetail',function (req,res,next) {
+  res.render('joinReceptionDetail');
 })
-router.get('/joinDetail',function (req,res,next) {
-  res.render('joinDetail');
-})
-router.get('/joinReception',function (req,res,next) {
-  res.render('joinReception');
+router.get('/joinReceptionStudentList',function (req,res,next) {
+  res.render('joinReceptionStudentList');
 })
 router.get('/joinReceptionStudentDetail',function (req,res,next) {
   res.render('joinReceptionStudentDetail');
 })
+router.get('/joinReceptionTodayCourse',function (req,res,next) {
+  res.render('joinReceptionTodayCourse');
+})
+router.get('/joinReceptionPrint',function (req,res,next) {
+  res.render('joinReceptionPrint');
+})
+router.get('/joinReceptionCourseCalendar',function (req,res,next) {
+  res.render('joinReceptionCourseCalendar');
+})
+router.get('/joinReceptionCourseManager',function (req,res,next) {
+  res.render('joinReceptionCourseManager');
+})
+
 router.get('/studentCourse',function (req,res,next) {
   res.render('studentCourse');
-})
-router.get('/studentList',function (req,res,next) {
-  res.render('studentList');
 })
 router.get('/studentDetail',function (req,res,next) {
   res.render('studentDetail');
@@ -35,6 +43,7 @@ router.get('/studentDetail',function (req,res,next) {
 router.get('/studentCourseDetail',function (req,res,next) {
   res.render('studentCourseDetail')
 })
+
 router.get('/teacherCourse',function (req,res,next) {
   res.render('teacherCourse')
 })
@@ -47,11 +56,10 @@ router.get('/teacherList',function (req,res,next) {
 router.get('/teacherDetail',function (req,res,next) {
   res.render('teacherDetail');
 })
-router.get('/courseList',function (req,res,next) {
-  res.render('courseList');
-})
-router.get('/courseDetail',function (req,res,next) {
-  res.render('courseDetail');
+
+
+router.get('/joinList',function (req,res,next) {
+  res.render('joinList');
 })
 router.get('/createCourse',function (req,res,next) {
   res.render('createCourse');
