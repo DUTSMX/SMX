8257c1bbe4d3c238d9468828ae5870991e21119c
18742021291 <wall1003@qq.com>
2017-08-15 19:43:51 +0800

    000

--- newAdmin/model/joinreceptionmanager.js
+++ newAdmin/model/joinreceptionmanager.js
@@ -4,21 +4,8 @@
 var Sequelize = require('sequelize');
 var course=require('./course');
 var user=require('./user');
-var sequelize=new Sequelize(
-    'smx',
-    'cdb_outerroot',
-    'smxsjk123456',
-    {
-        host: '59432c47c3382.bj.cdb.myqcloud.com',
-        port: 4243,
-        dialect: 'mysql',
-        define: {
-            underscored: false,
-            timestamps: false,
-            paranoid: true,
-        }
-    }
-);
+var db = require("./db")
+var sequelize = db.sequelize;
 var joinreceptionmanager = sequelize.define('joinreceptionmanager', {
         shopId:{ //自增长店铺Id,主键,整型
             type:Sequelize.INTEGER,
